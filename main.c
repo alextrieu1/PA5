1cbd838f9af8ce89515deb962ea1441446bd972b
alextrieu1 <trieualex09@gmail.com>
2026-04-12 18:26:07 -0400

    Created the createCat function

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ This program is written by: Alex Trieu*/
 #include <string.h>
 #define MAX_NAME 25
 #define NUM_TRAITS 5
+const char *TRAIT_NAMES[NUM_TRAITS] = {"friendly", "grumpy", "playful", "lazy", "curious"};
 
 typedef struct Cat{
     char *name; //dynamically allocate space for a cat's name
@@ -17,7 +18,65 @@ typedef struct BST_Node{
     Cat *cat;
     struct BST_Node *left;
     struct BST_Node *right;
-    int subtree_size; // stores the current sixe of this subtree's root(not height)
+    int subtree_size; // stores the current size of this subtree's root(not height)
 }BST_Node;
 
-const char *TRAIT_NAMES[NUM_TRAITS] = {"friendly", "grumpy", "playful", "lazy", "curious"};
+Cat* createCat(void); //Creates a Cat and returns it
+
+Cat* createCat(void){
+    char name[MAX_NAME], breed[MAX_NAME];
+    int charm; 
+    Cat* newCat = malloc(sizeof(Cat));
+
+    //Getting input
+    scanf("%s %s %d", name, breed, &charm);
+
+    //Dynamically allocated memory for cat name and breed
+    newCat->name = malloc((strlen(name) + 1) * sizeof(char));
+    newCat->breed = malloc((strlen(breed) + 1) * sizeof(char));
+    strcpy(newCat->name, name);
+    strcpy(newCat->breed, breed);
+    newCat->charm = charm;
+
+    //Getting traits
+    for(int i = 0; i < NUM_TRAITS; i++){
+        int trait;
+        scanf("%d", &trait);
+        newCat->traits[i] = trait; 
+    }
+
+    return newCat;
+}
+
+
+
+
+
+
+
+
+
+int main(void){
+    int query, n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", query);
+        if(query == 1){
+            
+
+        }else if (query == 2){
+        
+        }else if (query == 3){
+        
+        }else if (query == 4){
+        
+        }else if (query == 5){
+        
+        }else if (query == 6){
+        
+        }
+    }
+    
+    
+    return 0;
+}
