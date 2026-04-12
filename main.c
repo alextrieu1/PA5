19d6cec277466a7800f753676caddfd825fdac80
alextrieu1 <trieualex09@gmail.com>
2026-04-12 19:28:19 -0400

    Created the createNode, insert, and compareTrait functions

--- main.c
+++ main.c
@@ -21,12 +21,15 @@ typedef struct BST_Node{
     int subtree_size; // stores the current size of this subtree's root(not height)
 }BST_Node;
 
-Cat* createCat(void); //Creates a Cat and returns it
+Cat* createCat(void); //Creates a Cat, itializes its values and returns it
+BST_Node* createNode(Cat* value); //Creates a node, initializes its values and returns it
+BST_Node* insert(BST_Node* root, BST_Node* value); //Inserts a node into BST
+BST_Node* compareTraits(BST_Node* root, BST_Node* value); //Compares the amount of traits set to 1 and returns the node with the most
 
 Cat* createCat(void){
     char name[MAX_NAME], breed[MAX_NAME];
     int charm; 
-    Cat* newCat = malloc(sizeof(Cat));
+    Cat *newCat = malloc(sizeof(Cat));
 
     //Getting input
     scanf("%s %s %d", name, breed, &charm);
@@ -48,7 +51,74 @@ Cat* createCat(void){
     return newCat;
 }
 
+BST_Node* createNode(Cat* value){
 
+    //Allocate space for the node
+    BST_Node* temp;
+    temp = malloc(sizeof(BST_Node));
+
+    //Initialize the fields
+    temp->cat = value;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->subtree_size = 0;
+
+    return temp;
+}
+
+BST_Node* insert(BST_Node* root, BST_Node* value){
+    if(root == NULL)
+        return value;
+    else{
+
+        //element should be inserted to the right
+        if(strcmp(value->cat->name, root->cat->name) < 0){
+            if(root->right != NULL)    
+                root->right = insert(root->right, value);
+            else
+                root->right = value;
+        } else if(strcmp(value->cat->name, root->cat->name) > 0) {
+            //element should be inserted to the left
+            if(root->left != NULL)
+                root->left = insert(root->left, value);
+            else 
+                root->left = value;
+        } else {
+            //root and value have the same name
+            root = compareTraits(root, value);
+        }
+    } 
+
+    return root;
+}
+
+BST_Node* compareTraits(BST_Node* root, BST_Node* value){
+    Cat* t1 = malloc(sizeof(Cat));
+    Cat* t2 = malloc(sizeof(Cat));
+
+    //Counters
+    int n1 = 0, n2 = 0;
+    t1 = root->cat;
+    t2 = value->cat;
+
+    //Finding number of traits 
+    for(int i = 0; i < NUM_TRAITS; i++){
+        if(t1->traits[i] == 1)
+            n1++;
+        if(t2->traits[i] == 1)
+            n2++;
+    }
+
+    //Freeing temp cats
+    free(t1);
+    free(t2);
+
+    //The existing node is replaced only if the new cat has strictly more traits set to 1
+    if(n1 >= n2)
+        return root;
+    else    
+        return value;
+}
 
 
 
@@ -62,7 +132,8 @@ int main(void){
     for(int i = 0; i < n; i++){
         scanf("%d", query);
         if(query == 1){
-            
+            Cat *newCat = createCat(); //Create the cat
+            BST_Node *newNode = createNode(newCat); //Create the node
 
         }else if (query == 2){
         
